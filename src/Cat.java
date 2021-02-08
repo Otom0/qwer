da8a88ae589b53df4096c6cc8cfa029eb42de1cc
Robert Dolgopolov <siidousdart@gmail.com>
2021-02-08 23:32:55 +0500

    na nahui

---
+++ src/Cat.java
@@ -0,0 +1,56 @@
+
+public class Cat
+{
+    private double originWeight;
+    private double weight;
+
+    private double minWeight;
+    private double maxWeight;
+
+    public Cat()
+    {
+        weight = 1500.0 + 3000.0 * Math.random();
+        originWeight = weight;
+        minWeight = 1000.0;
+        maxWeight = 9000.0;
+
+    }
+
+    public boolean meow()
+    {
+        weight = weight - 100;
+        System.out.println("Meow");
+        return false;
+    }
+
+    public void feed(Double amount)
+    {
+        weight = weight + amount;
+    }
+
+    public void drink(Double amount)
+    {
+        weight = weight + amount;
+    }
+
+    public Double getWeight()
+    {
+        return weight;
+    }
+
+    public String getStatus()
+    {
+        if(weight < minWeight) {
+            return "Dead";
+        }
+        else if(weight > maxWeight) {
+            return "Exploded";
+        }
+        else if(weight > originWeight) {
+            return "Sleeping";
+        }
+        else {
+            return "Playing";
+        }
+    }
+}
---
+++ src/Loader.java
@@ -0,0 +1,42 @@
+import javafx.scene.text.FontWeight;
+
+public class Loader
+{
+    public static void main(String[] args)
+    {
+        Cat cat1 = new Cat();
+        Cat cat2 = new Cat();
+        Cat cat3 = new Cat();
+        Cat cat4 = new Cat();
+        Cat cat5 = new Cat();
+
+        System.out.println("qwe " + cat1.getWeight());
+        System.out.println("asd " + cat2.getWeight());
+        System.out.println("zxc " + cat3.getWeight());
+        System.out.println("qaz " + cat4.getWeight());
+        System.out.println("wsx " + cat5.getWeight());
+
+        cat1.feed((double) 1);
+        System.out.println("qwe " + cat1.getWeight());
+
+        cat2.feed((double) 1);
+        System.out.println("asd " + cat2.getWeight());
+
+        System.out.println("\n=====================================================\n");
+
+//        while (cat5.getWeight() <=9000) {
+//            cat5.feed((double) 1);
+//
+//        }
+//        System.out.println("qwer " + cat5.getStatus());
+//
+//
+//            while (cat4.getWeight() >= 1000) {
+//
+//                cat4.meow();
+//            }
+//
+//        System.out.println("ХУЙ " + cat4.getStatus());
+
+    }
+}
