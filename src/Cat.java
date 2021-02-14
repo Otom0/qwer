e4bc1b4848c32020830746e430117d521c865cdb
Robert Dolgopolov <siidousdart@gmail.com>
2021-02-14 23:21:49 +0500

    пожрал поссал

--- src/Cat.java
+++ src/Cat.java
@@ -1,45 +1,71 @@
 
-public class Cat
-{
+public class Cat {
     private double originWeight;
     private double weight;
-
+    public static int count;
     private double minWeight;
     private double maxWeight;
     private double eatable;
 
 
-
-    public Cat()
-    {
+    public Cat() {
         weight = 1500.0 + 3000.0 * Math.random();
         originWeight = weight;
         minWeight = 1000.0;
         maxWeight = 9000.0;
-
+        count++;
 
     }
 
 
-    public boolean meow()
+    public void meow()
     {
-        weight = weight - 100;
-        System.out.println("Meow");
-        return false;
+        if (getStatus().equals("Dead") || getStatus().equals("Exploded"))
+        {
+
+            System.out.println("can't meow");
+        } else
+            {
+            weight = weight - 100;
+            System.out.println("Meow");
+
+        }
+
+
     }
-    public void pee(Double amount){ weight = weight + amount;
-        System.out.println("пописал");
+    public void pee(Double amount){
+        if (getStatus().equals("Dead") || getStatus().equals("Exploded")) {
+
+            System.out.println("can't pee");
+        }
+        else {
+            weight = weight + amount;
+
+            System.out.println("пописал");
+        }
     }
 
 
     public void feed(Double amount)
     {
-        weight = weight + amount;
+        if (getStatus().equals("Dead") || getStatus().equals("Exploded")) {
+
+            System.out.println("can't feed");
+        }
+        else {
+            weight = weight + amount;
+        }
     }
 
     public void drink(Double amount)
     {
-        weight = weight + amount;
+        if (getStatus().equals("Dead") || getStatus().equals("Exploded")) {
+
+            System.out.println("can't drink");
+        }
+        else {
+            weight = weight + amount;
+        }
     }
 
     public Double getWeight()
@@ -47,12 +73,15 @@ public class Cat
         return weight;
     }
 
+
     public String getStatus()
     {
         if(weight < minWeight) {
+            count--;
             return "Dead";
         }
         else if(weight > maxWeight) {
+            count--;
             return "Exploded";
         }
         else if(weight > originWeight) {
@@ -66,5 +95,9 @@ public class Cat
     public double getEatable() {
         eatable = weight - originWeight;
         return eatable;
+    }
+
+    public static double getCount() {
+        return count;
     }
 }
--- src/Loader.java
+++ src/Loader.java
@@ -4,47 +4,62 @@ public class Loader
 {
     public static void main(String[] args)
     {
+        //System.out.println("Dead".equals("Dead"));
         Cat cat1 = new Cat();
         Cat cat2 = new Cat();
         Cat cat3 = new Cat();
         Cat cat4 = new Cat();
         Cat cat5 = new Cat();
+        System.out.println("Количество котов " +  Cat.getCount());
+        //if ()
 
         System.out.println("cat1 " + cat1.getWeight());
-        cat1.feed((double)10);
+        cat1.feed((double)100);
         System.out.println(cat1.getEatable());
         System.out.println("cat1 " + cat1.getWeight());
 
         cat1.pee((double) 10);
-        System.out.println("cat1 " + cat1.getWeight());
+        System.out.println("hgj " + cat1.getWeight());
+        System.out.println("asd " + cat2.getWeight());
+        System.out.println("zxc " + cat3.getWeight());
+        System.out.println("qaz " + cat4.getWeight());
+        System.out.println("wsx " + cat5.getWeight());
+
+        cat3.feed((double) 1);
+        System.out.println("qwe " + cat3.getWeight());
+
+        cat2.feed((double) 1);
+        System.out.println("asd " + cat2.getWeight());
+
+        System.out.println("\n=====================================================\n");
+
+        while (cat5.getWeight() <=9000) {
+            cat5.feed((double) 1);
+
+        }
+        System.out.println("qwer " + cat5.getStatus());
 
 
-//        System.out.println("asd " + cat2.getWeight());
-//        System.out.println("zxc " + cat3.getWeight());
-//        System.out.println("qaz " + cat4.getWeight());
-//        System.out.println("wsx " + cat5.getWeight());
+            while (cat4.getWeight() >= 1000) {
 
-//        cat1.feed((double) 1);
-//        System.out.println("qwe " + cat1.getWeight());
-//
-//        cat2.feed((double) 1);
-//        System.out.println("asd " + cat2.getWeight());
-//
-//        System.out.println("\n=====================================================\n");
+                cat4.meow();
+            }
 
-//        while (cat5.getWeight() <=9000) {
-//            cat5.feed((double) 1);
-//
-//        }
-//        System.out.println("qwer " + cat5.getStatus());
-//
-//
-//            while (cat4.getWeight() >= 1000) {
-//
-//                cat4.meow();
-//            }
-//
-//        System.out.println("ХУЙ " + cat4.getStatus());
+        System.out.println("[pop] " + cat4.getStatus());
+
+        System.out.println("Количество котов " +  Cat.getCount());
+
+        cat4.pee((double) 10);
+        cat4.feed((double) 1);
+        cat4.meow();
+        cat4.drink((double) 1);
+        cat5.meow();
+        cat5.drink((double) 1);
+        cat5.pee((double) 1);
+        cat5.feed((double) 1);
+
+        //if (cat1.getStatus().equals("Exploded")) System.out.println("Ymer");
+
 
     }
 }
